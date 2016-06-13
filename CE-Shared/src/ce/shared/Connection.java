b218ad41e52c08d6e689f75e3e02ecc1440f9f0b
FloLod <Florian.Loddenkemper@gmx.de>
2016-06-13 11:39:49 +0200

    commit

--- CE-Shared/src/ce/shared/Connection.java
+++ CE-Shared/src/ce/shared/Connection.java
@@ -3,7 +3,6 @@ package ce.shared;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.util.function.Consumer;
 
