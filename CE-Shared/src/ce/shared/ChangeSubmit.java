617eaa4fa792329bea337ae305df808a2757a6b2
FloLod <Florian.Loddenkemper@gmx.de>
2016-06-16 10:00:17 +0200

    com

--- CE-Shared/src/ce/shared/ChangeSubmit.java
+++ CE-Shared/src/ce/shared/ChangeSubmit.java
@@ -1,15 +1,21 @@
 package ce.shared;
 
 public class ChangeSubmit {
-	public final String text;
+	private final String text;
+	private final long serverID;
 
-	public ChangeSubmit(String text) {
+	public ChangeSubmit(String text, long serverID) {
 		super();
 		this.text = text;
+		this.serverID = serverID;
 	}
 
 	public String getText() {
 		return this.text;
+	}
+	
+	public long getServerID(){
+		return this.serverID;
 	}
 
 }
